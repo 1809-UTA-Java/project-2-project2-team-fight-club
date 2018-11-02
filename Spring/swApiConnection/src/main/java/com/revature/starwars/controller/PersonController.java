cc7e5b071f93079af51701facdd3f22fd567bedc
Cody Phillips <cphill38@yahoo.com>
2018-11-01 22:27:46 -0500

    Random SW character w/ random battle rank returned

--- Spring/swApiConnection/src/main/java/com/revature/starwars/controller/PersonController.java
+++ Spring/swApiConnection/src/main/java/com/revature/starwars/controller/PersonController.java
@@ -7,35 +7,44 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.revature.starwars.component.CallSwapi;
+import com.revature.starwars.component.PersonComponent;
+import com.revature.starwars.model.Person;
 
 @RestController
+@CrossOrigin
 public class PersonController {
-	
-	CallSwapi callSwapi;
+
+	PersonComponent personComponent;
 	RestTemplate restTemplate = new RestTemplate();
-	
+
 	@Autowired
-	public PersonController(CallSwapi callSwapi) {
+	public PersonController(PersonComponent personComponent) {
 		super();
-		this.callSwapi = callSwapi;
+		this.personComponent = personComponent;
 	}
-	
-	@RequestMapping("/get") 
-	public Object getPerson() { // Returns to browser JSON object of person from SWAPI
+
+	@RequestMapping("/get")
+	public Person getPerson() { // Returns to browser JSON object of person from
+								// SWAPI
+		int index = personComponent.random(87);
+		String url = "https://swapi.co/api/people/" + index + "/";
+
 		HttpHeaders httpHeader = new HttpHeaders();
 		httpHeader.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-		httpHeader.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+		httpHeader.add("user-agent",
+				"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
 		HttpEntity<String> entity = new HttpEntity<String>("parameters", httpHeader);
-		Object person = restTemplate.exchange("https://swapi.co/api/people/2/", HttpMethod.GET, entity, Object.class);
-		return person;
-	}
-	
+		Object obj = restTemplate.exchange(url, HttpMethod.GET, entity, Person.class);
 
-	
+		Person person = personComponent.createPerson(obj.toString(), url);
+
+		return person;
+
+	}
 
 }
--- Spring/swApiConnection/src/main/java/com/revature/starwars/model/Person.java
+++ Spring/swApiConnection/src/main/java/com/revature/starwars/model/Person.java
@@ -1,56 +1,37 @@
 package com.revature.starwars.model;
 
-import java.util.List;
+import java.util.UUID;
 
-import javax.xml.bind.annotation.XmlRootElement;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
- 
-@XmlRootElement
-@JsonIgnoreProperties(ignoreUnknown=true)
+@Entity
 public class Person {
 
+	@Id
+	private UUID id;
+
 	private String name;
-	private String height;
-	private String mass;
-	private String hairColor;
-	private String skinColor;
-	private String eyeColor;
-	private String birthYear;
-	private String gender;
-	private String homeWorld;
-	private List<String> films;
-	private List<String> species;
-	private List<String> vehicles;
-	private List<String> starships;
-	private String created;
-	private String edited;
+	private int battleRank;
 	private String url;
 
 	public Person() {
 		super();
 	}
 
-	public Person(String name, String height, String mass, String hairColor, String skinColor, String eyeColor,
-			String birthYear, String gender, String homeWorld, List<String> films, List<String> species,
-			List<String> vehicles, List<String> starships, String created, String edited, String url) {
+	public Person(String name, String url) {
 		super();
+		this.id = UUID.randomUUID();
 		this.name = name;
-		this.height = height;
-		this.mass = mass;
-		this.hairColor = hairColor;
-		this.skinColor = skinColor;
-		this.eyeColor = eyeColor;
-		this.birthYear = birthYear;
-		this.gender = gender;
-		this.homeWorld = homeWorld;
-		this.films = films;
-		this.species = species;
-		this.vehicles = vehicles;
-		this.starships = starships;
-		this.created = created;
-		this.edited = edited;
 		this.url = url;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
 	}
 
 	public String getName() {
@@ -61,116 +42,12 @@ public class Person {
 		this.name = name;
 	}
 
-	public String getHeight() {
-		return height;
+	public int getBattleRank() {
+		return battleRank;
 	}
 
-	public void setHeight(String height) {
-		this.height = height;
-	}
-
-	public String getMass() {
-		return mass;
-	}
-
-	public void setMass(String mass) {
-		this.mass = mass;
-	}
-
-	public String getHairColor() {
-		return hairColor;
-	}
-
-	public void setHairColor(String hairColor) {
-		this.hairColor = hairColor;
-	}
-
-	public String getSkinColor() {
-		return skinColor;
-	}
-
-	public void setSkinColor(String skinColor) {
-		this.skinColor = skinColor;
-	}
-
-	public String getEyeColor() {
-		return eyeColor;
-	}
-
-	public void setEyeColor(String eyeColor) {
-		this.eyeColor = eyeColor;
-	}
-
-	public String getBirthYear() {
-		return birthYear;
-	}
-
-	public void setBirthYear(String birthYear) {
-		this.birthYear = birthYear;
-	}
-
-	public String getGender() {
-		return gender;
-	}
-
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-
-	public String getHomeWorld() {
-		return homeWorld;
-	}
-
-	public void setHomeWorld(String homeWorld) {
-		this.homeWorld = homeWorld;
-	}
-
-	public List<String> getFilms() {
-		return films;
-	}
-
-	public void setFilms(List<String> films) {
-		this.films = films;
-	}
-
-	public List<String> getSpecies() {
-		return species;
-	}
-
-	public void setSpecies(List<String> species) {
-		this.species = species;
-	}
-
-	public List<String> getVehicles() {
-		return vehicles;
-	}
-
-	public void setVehicles(List<String> vehicles) {
-		this.vehicles = vehicles;
-	}
-
-	public List<String> getStarships() {
-		return starships;
-	}
-
-	public void setStarships(List<String> starships) {
-		this.starships = starships;
-	}
-
-	public String getCreated() {
-		return created;
-	}
-
-	public void setCreated(String created) {
-		this.created = created;
-	}
-
-	public String getEdited() {
-		return edited;
-	}
-
-	public void setEdited(String edited) {
-		this.edited = edited;
+	public void setBattleRank(int battleRank) {
+		this.battleRank = battleRank;
 	}
 
 	public String getUrl() {
@@ -183,7 +60,7 @@ public class Person {
 
 	@Override
 	public String toString() {
-		return "People [name=" + name + ", birthYear=" + birthYear + ", homeWorld=" + homeWorld + ", url=" + url + "]";
+		return "Person [id=" + id + ", name=" + name + ", battleRank=" + battleRank + ", url=" + url + "]";
 	}
 
 }
