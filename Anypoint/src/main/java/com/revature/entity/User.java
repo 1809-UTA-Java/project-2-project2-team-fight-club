7e08237521c557e8eef818b55ea0edb4f4df303a
Ricardo Aranda <ricardoaranda@Ricardos-MacBook-Pro.local>
2018-11-03 17:19:52 -0500

    small changes

--- Anypoint/src/main/java/com/revature/entity/User.java
+++ Anypoint/src/main/java/com/revature/entity/User.java
@@ -1,9 +1,11 @@
 package com.revature.entity;
 
+import org.mule.api.MuleEventContext;
+import org.mule.api.lifecycle.Callable;
 import org.springframework.stereotype.Component;
 
 @Component
-public class User {
+public class User implements Callable{
 	private String id;
 	private String name;
 
@@ -34,6 +36,12 @@ public class User {
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + "]";
+	}
+
+	@Override
+	public Object onCall(MuleEventContext eventContext) throws Exception {
+		
+		return null;
 	}
 
 }
